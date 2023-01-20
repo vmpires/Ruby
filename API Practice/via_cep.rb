b5c53da3c705daceff1e34c7b65dcba8faeb9cf1
Victor Milhome Pires <vctprs@gmail.com>
2023-01-19 21:44:35 -0300

    using nethttp instead of rest client

--- API Practice/via_cep.rb
+++ API Practice/via_cep.rb
@@ -1,14 +1,12 @@
-require 'rest-client'
+require 'net/http'
 require 'json'
 
 puts "Insert your postal code: "
 cep = gets.chomp.strip.gsub('-','')
 
-url = "https://viacep.com.br/ws/#{cep}/json/"
+response = Net::HTTP.get('viacep.com.br', "/ws/#{cep}/json/")
 
-response = RestClient.get(url, headers={})
-
-cep_hash = JSON.parse(response.body)
+cep_hash = JSON.parse(response)
 
 puts "You live in #{cep_hash['logradouro']}, #{cep_hash['complemento']}
 #{cep_hash['bairro']} - #{cep_hash['localidade']}/#{cep_hash['uf']}"
